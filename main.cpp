48e4736790656c768822b5720ca14a58627827cf
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-03-24 22:37:00 +0800

    moved back to cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <stdlib.h>
+#include <string>
+#define MAX_INPUT 1000
 
 int main(int argc, char **argv) {
   float rootFontSize = 16.0;
   if (argc == 2) {
-    rootFontSize = atof(argv[1]);
+    rootFontSize = std::stod(std::string(argv[1]));
   } else if (argc > 2) {
-    std::cout << "rem-calc: expected at most 1 argument, but got " << argc
-              << std::endl;
+    std::cout << "rem-calc: expected at most 1 argument, but got " << (argc - 1)
+              << " instead." << std::endl;
     return 1;
   }
 
@@ -16,5 +17,6 @@ int main(int argc, char **argv) {
     std::cin >> value;
     std::cout << (value / rootFontSize) << "rem" << std::endl;
   }
+
   return 0;
 }
